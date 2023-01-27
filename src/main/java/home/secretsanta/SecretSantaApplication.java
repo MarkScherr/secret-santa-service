6a87f02832a473c8682e7b543345ccd280baddfd
MarkScherr <mrscherr@gmail.com>
2023-01-26 23:07:04 -0600

    update main class

--- src/main/java/home/secretsanta/SecretSantaApplication.java
+++ src/main/java/home/secretsanta/SecretSantaApplication.java
@@ -3,13 +3,12 @@ package home.secretsanta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class SecretSantaApplication {
